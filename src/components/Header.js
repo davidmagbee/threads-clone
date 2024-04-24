2d58bec2d61257480799ede32417909190d4dde2
“davidmagbee” <david.magbee@me.com>
2024-04-24 16:19:43 -0400

    layout, buttons, header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <header className='header'>Header</header>
-  )
-}
+    <header className="header">
+      <div className="info-container">
+        <div className="user-info-container">
+          <h1>username</h1>
+          <p>
+            handle <span className="threads-info">threads.net</span>
+          </p>
+        </div>
+        <div className="avatar-container">
+          <img src="" alt="user avatar" />
+        </div>
+      </div>
+      <p>bio</p>
+      <div className="sub-info-container">
+        <p className="sub-text">
+          # of followers &#x2022; <a href="">user link</a>{" "}
+        </p>
+      </div>
+      <button
+        className="primary"
+        onClick={() => navigator.clipboard.writeText("URL for user")}
+      >
+        Share Profile
+      </button>
+      <div className="button-container">
+        <button>Threads</button>
+        <button>Replies</button>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
