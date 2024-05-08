82c11b59012df4de8dff2ff6576ca33760eda1e7
“davidmagbee” <david.magbee@me.com>
2024-05-08 12:15:06 -0400

    functionality for getting and setting threads in Header

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import Nav from "./components/Nav";
 
 const App = () => {
   const [user, setUser] = useState(null);
+  const [threads, setThreads] = useState(null);
+  const [viewThreadsFeed, setViewThreadsFeed] = useState(true);
+  const [filteredThreads, setFilteredThreads] = useState(null);
 
   // temporary user id hardcoded for testing
   const userId = "c6f29f62-ae45-4853-87d4-33e2c5a87be9";
@@ -25,9 +28,37 @@ const App = () => {
     }
   };
 
+  const getThreads = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/threads?thread_from=${userId}`
+      );
+      const data = await response.json();
+      setThreads(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const getThreadFeed = async () => {
+    if (viewThreadsFeed) {
+      const standAloneThreads = threads?.filter((thread) => thread.reply_to === null)
+      setFilteredThreads(standAloneThreads)
+    }
+    if (!viewThreadsFeed) {
+    const replyThreads = threads?.filter((thread) => thread.reply_to!== null)
+    setFilteredThreads(replyThreads)
+    }
+  }
+
   useEffect(() => {
     getUser();
+    getThreads();
   }, []);
+
+  useEffect(() => {
+    getThreadFeed()
+  }, [user, threads, viewThreadsFeed])
 
   return (
     <div>
@@ -37,6 +68,8 @@ const App = () => {
 
           <Header 
             user={user}
+            viewThreadsFeed={viewThreadsFeed}
+            setViewThreadsFeed={setViewThreadsFeed}
           />
 
           <Feed />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Header = ({ user }) => {
+const Header = ({ user, viewThreadsFeed, setViewThreadsFeed }) => {
   return (
     <header className="header">
       <div className="info-container">
@@ -27,8 +27,8 @@ const Header = ({ user }) => {
         Share Profile
       </button>
       <div className="button-container">
-        <button>Threads</button>
-        <button>Replies</button>
+        <button className={viewThreadsFeed ? "current": null} onClick={() => setViewThreadsFeed(true)}>Threads</button>
+        <button className={!viewThreadsFeed ? "current": null} onClick={() => setViewThreadsFeed(false)}>Replies</button>
       </div>
     </header>
   );
